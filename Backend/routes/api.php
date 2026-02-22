5caeedb0e0986e4d0daed88784b64e7d6290b414
MierafA12 <mierafabebe12@gmail.com>
2026-02-22 20:08:49 +0300

    create api end points to register users

---
+++ Backend/routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+require __DIR__.'/api/auth.php';
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user()->load('profile');
+    });
+});
