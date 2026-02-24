1f59be1814904d0ce02596589697433085c86616
MierafA12 <mierafabebe12@gmail.com>
2026-02-24 22:15:41 +0300

    api updated

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 require __DIR__.'/api/auth.php';
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user()->load('profile');
     });
