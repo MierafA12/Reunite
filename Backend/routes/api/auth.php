b1f9041e7f6449a3b06ea81b6c4e3636a5d36495
MierafA12 <mierafabebe12@gmail.com>
2026-02-24 22:14:52 +0300

    api creation for verification

--- Backend/routes/api/auth.php
+++ Backend/routes/api/auth.php
@@ -1,7 +1,18 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::post('register', [AuthController::class, 'register']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/email/verify/{id}/{hash}', [VerifyEmailController::class, 'verify'])
+        ->middleware(['signed'])
+        ->name('verification.verify');
+
+    Route::post('/email/verification-notification', [VerifyEmailController::class, 'resend'])
+        ->middleware(['throttle:6,1'])
+        ->name('verification.send');
+});
