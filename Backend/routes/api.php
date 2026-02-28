ae848a8e4bbc46d04c1bab1aa2badb72a35572e8
MierafA12 <mierafabebe12@gmail.com>
2026-02-28 20:50:50 +0300

    added profile endpoints in the api

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -3,10 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Resources\UserResource;
+
 require __DIR__.'/api/auth.php';
 
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('/user', function (Request $request) {
-        return $request->user()->load('profile');
+        return new UserResource($request->user()->load('profile'));
     });
+    Route::get('/profile', [ProfileController::class, 'show']);
+    Route::get('/profile/{user}', [ProfileController::class, 'showPublic']);
 });
